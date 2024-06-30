bba1015ab90187b2697cb9f1b15ffebb70b9bc4c
amieecode <enwelumchiamaka@gmail.com>
2024-06-30 19:31:48 +0100

    Added Pagination to the post views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from.models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list_ = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render( request, 'blog/post/list.html', {'posts':posts} )
 
-def post_detail(request, id):
-    post = get_object_or_404( Post, id=id, status=Post.Status.PUBLISHED )
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404( Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day )
     return render( request, 'blog/post/detail.html', {'post': post} )
