262d1a98cae0fce929412f1fc70bdcbe2d2eefc4
amieecode <enwelumchiamaka@gmail.com>
2024-07-04 21:39:51 +0100

    Added Pagination to the blog post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,12 +4,12 @@ from.models import Post
 
 # Create your views here.
 def post_list(request):
-    post_list_ = Post.published.all()
+    post_list = Post.published.all()
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     posts = paginator.page(page_number)
-    return render( request, 'blog/post/list.html', {'posts':posts} )
+    return render( request, 'blog/post/list.html', {'posts': posts} )
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404( Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day )
