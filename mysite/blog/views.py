1bf9dbe27750cde94237cb6f509b06d5d7b60d9e
amieecode <enwelumchiamaka@gmail.com>
2024-07-06 13:40:26 +0100

    created a form and handled the forms in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.generic import ListView 
-from.models import Post
+from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -13,3 +14,17 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404( Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day )
     return render( request, 'blog/post/detail.html', {'post': post} )
+
+def post_share(request, post_id):
+    #Retrieve post by id 
+    post = get_object_or_404( Post, id=post_id, status=Post.Status.PUBLISHED )
+    if request.method == 'POST':
+        #Form was submitted from
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #form field passed validation
+            cd = form.cleaned_data
+    #...Send email 
+    else:
+        form = EmailPostForm()
+    return ( request, 'blog/post/share.html', {'post': post, 'form': form } )
