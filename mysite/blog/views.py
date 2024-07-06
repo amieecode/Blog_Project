505adb9c2c2934368a5c735d8b8666c5a79ff7f2
amieecode <enwelumchiamaka@gmail.com>
2024-07-06 13:06:54 +0100

    Worked on using a class based view to list posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView 
 from.models import Post
 
 # Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 posts per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render( request, 'blog/post/list.html', {'posts': posts} )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by =3 
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404( Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day )
