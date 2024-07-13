f0afc4f1a80a8d4834b31f01b4e711fc3012f45c
amieecode <enwelumchiamaka@gmail.com>
2024-07-13 01:34:09 +0100

    created a template tag that returns a queryset

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView 
 from django.core.mail import send_mail
+from django.db.models import Count
 from .models import Post
 from .forms import CommentForm, EmailPostForm
 from taggit.models import Tag
@@ -20,7 +21,10 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
-    return render( request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form } )
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter( tags__in=post_tags_ids ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags') ).order_by('-same_tags', '-publish')[:4]
+    return render( request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts } )
 
 def post_list(request, tag_slug=None):
     post_list = Post.published.all()
